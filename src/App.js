66910dbc121d567de49cddb8caa2c692b9a80a3b
maris <marisandb@yahoo.com>
2021-10-16 22:07:34 -0500

    added contact me form and nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import About from './components/About'
+import Contact from './components/Contact'
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav>
+      </Nav>
       <main>
         <About></About>
+        <Contact></Contact>
       </main>
     </div>
   );
